401a347917d7f2280511473f216f2c587f7fa74b
Mark Kendall <mkendall@mythtv.org>
2009-08-27 09:29:43 +0000

    Enable hdhomerun support in the win32-packager. Missed in r21543.
    git-svn-id: http://svn.mythtv.org/svn/trunk@21544 7dbf422c-18fa-0310-86e9-fd20926502f2

--- Win32/build/win32-packager.pl
+++ Win32/build/win32-packager.pl
@@ -1628,7 +1628,7 @@ push @{$expect},
   shell  => ['source '.$unixmythtv.'qt'.$qtver.'_env.sh',
             'cd '.$unixmythtv.'mythtv',
             './configure --prefix='.$unixbuild.' --runprefix=..'.
-            ' --disable-dbox2 --disable-hdhomerun'.
+            ' --disable-dbox2'.
             ' --disable-iptv --disable-joystick-menu --disable-xvmc-vld'.
             ' --disable-xvmc --enable-directx --disable-lirc'.
             ' --cpu=pentium4 --compile-type='.$compile_type],
