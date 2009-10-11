7cd469d06d135de88c327ad4b28de85abcd6ddad
Mario Limonciello <superm1@ubuntu.com>
2009-10-11 11:44:03 -0500

    * Ship a mythbuntu crashdb for apport to catch crashes and submit against the   mythbuntu project. * Add an apport hook for mythtv packages.   - PPA builds will be directed at launchpad.net/mythbuntu   - Normal builds will end up against launchpad.net/ubuntu/+source/mythtv

---
+++ debian/source_mythtv.py
@@ -0,0 +1,27 @@
+import os.path, os
+import subprocess
+import apport.hookutils
+
+def is_ppa(pkg):
+    script = subprocess.Popen(['apt-cache', 'policy', pkg], stdout=subprocess.PIPE)
+    output = script.communicate()[0]
+    return 'mythbuntu' in output.split('\n')[1].replace("Installed: ", "")
+
+def add_info(report):
+    logs = [ '/var/log/mythtv/mythbackend.log',
+             '/var/log/mythtv/mythfrontend.log',
+             '/var/log/mythtv/jamu.log',
+             '/proc/cpuinfo'
+           ]
+    for log in logs:
+        apport.hookutils.attach_file_if_exists(report, log)
+
+    if is_ppa('mythtv-common'):
+        report['CrashDB'] = 'mythbuntu'
+
+## DEBUGING ##
+if __name__ == '__main__':
+    report = {}
+    add_info(report)
+    for key in report:
+        print '[%s]\n%s' % (key, report[key])
