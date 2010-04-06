9cfe6428465074ae235c25bb3d2f1f2c1aabccca
Nigel Pearson <npearson@mythtv.org>
2010-04-06 08:46:58 +0000

    Up the default build revision. The previous one wouldn't build (usleep probs).
    git-svn-id: http://svn.mythtv.org/svn/trunk@24010 7dbf422c-18fa-0310-86e9-fd20926502f2

--- Win32/build/win32-packager.pl
+++ Win32/build/win32-packager.pl
@@ -44,7 +44,7 @@ $SIG{INT} = sub { die "Interrupted\n"; };
 $| = 1; # autoflush stdout;
 
 # this script was last tested to work with this version, on other versions YMMV.
-my $SVNRELEASE = '23037'; # Recent trunk
+my $SVNRELEASE = '23137'; # Recent trunk
 #my $SVNRELEASE = 'HEAD'; # If you are game, go forth and test the latest!
 
 
