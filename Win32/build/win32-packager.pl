b56f251352a3374313bf934857f55b1869cae630
Robert McNamara <rmcnamara@mythtv.org>
2010-10-19 17:33:03 +0000

    Remove redundant declaration of version in Win32 packaging script, bump trunk version to .24 until we start naming libs .25 closer to that release.  Refs #9115.
    git-svn-id: http://svn.mythtv.org/svn/trunk@26901 7dbf422c-18fa-0310-86e9-fd20926502f2

--- Win32/build/win32-packager.pl
+++ Win32/build/win32-packager.pl
@@ -67,7 +67,7 @@ my $proxy = '';
 # Subversion proxy settings are configured in %APPDATA%\Subversion\servers
 
 my $NOISY   = 1;            # Set to 0 for less output to the screen
-my $version = '0.23';       # Main mythtv version - used to name dlls
+my $version = '0.24';       # Main mythtv version - used to name dlls
 my $package = 0;            # Create a Win32 Distribution package? 1 for yes
 my $compile_type = "profile"; # compile options: debug, profile or release
 my $tickets = 0;            # Apply specific win32 tickets -
@@ -103,7 +103,6 @@ if (defined $opt{c}) {
 }
 
 if (defined $opt{b}) {
-    $version = "0.23.1";
     my @num = split /\./, $version;
     $svnlocation = "branches/release-$num[0]-$num[1]-fixes";
 } else {
