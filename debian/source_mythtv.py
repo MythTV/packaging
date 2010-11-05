645e94ea83e9672b8f6cfff70bf309001a7230ce
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-11-05 13:09:20 -0500

    Drop references to mythplugins-dbg in the apport rules.

--- debian/source_mythtv.py
+++ debian/source_mythtv.py
@@ -12,15 +12,11 @@ def add_info(report):
 
     report['MythTVDirectoryPermissions'] = apport.hookutils.command_output(['ls', '-l', '/var/lib/mythtv'])
 
-    dbg_packages = [ 'mythtv-dbg',
-                     'mythplugins-dbg'
-                   ]
-    for dbg in dbg_packages:
-        try:
-            status = apport.packaging.get_version(dbg)
-        except ValueError:
-            status = '0.0'
-        report["Installed_" + dbg] = status
+    try:
+        status = apport.packaging.get_version('mythtv-dbg')
+    except ValueError:
+        status = '0.0'
+    report["Installed_mythtv_dbg"] = status
 
     if report.has_key('Package') and not apport.packaging.is_distro_package(report['Package'].split()[0]):
         report['CrashDB'] = 'mythbuntu'
