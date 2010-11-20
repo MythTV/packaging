295344c1961ee8858a183f6426723b0e7a034be0
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-11-20 13:18:06 -0600

    also run dch for the automated uploader

--- debian/do-new-release.sh
+++ debian/do-new-release.sh
@@ -2,3 +2,9 @@ debian/rules update-latest-revision
 debian/rules get-svn-source
 debian/rules update-control-files
 debian/rules update-upstream-changelog
+if [ -n "$1" ]; then
+	DIST="$1"
+else
+	DIST="natty"
+fi
+dch -r -D $DIST --force-distribution ""
