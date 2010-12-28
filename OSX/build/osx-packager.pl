451f630ba3a5067f91f47dac8dff2876a6b76eee
Nigel Pearson <npearson@mythtv.org>
2010-12-29 09:27:42 +1100

    Correct mistake in [34f10715] - taglib version changed.

--- OSX/build/osx-packager.pl
+++ OSX/build/osx-packager.pl
@@ -108,7 +108,7 @@ our %depend = (
 
   'taglib' =>
   {
-    'url' => 'http://developer.kde.org/~wheeler/files/src/taglib-1.5.tar.gz',
+    'url' => 'http://developer.kde.org/~wheeler/files/src/taglib-1.6.3.tar.gz',
   },
 
   'libogg' =>
