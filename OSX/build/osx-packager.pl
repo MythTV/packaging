ab6f3d0ad582461fe22abd2962988b00e7d57103
Nigel Pearson <npearson@mythtv.org>
2011-01-05 19:47:54 +1100

    Enable preview generation by backend. Closes #9436. Thanks Britney!

--- OSX/build/osx-packager.pl
+++ OSX/build/osx-packager.pl
@@ -1120,7 +1120,7 @@ if ( $backend && grep(m/MythBackend/, @targets) )
 
     # The backend gets all the useful binaries it might call:
     foreach my $binary ( 'mythjobqueue', 'mythcommflag',
-                         'mythtranscode', 'mythfilldatabase' )
+                         'mythpreviewgen', 'mythtranscode', 'mythfilldatabase' )
     {
         my $SRC  = "$PREFIX/bin/$binary";
         if ( -e $SRC )
