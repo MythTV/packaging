e9f5527cab2c7b86e40f3875942cc1b4d0671c99
Stuart Auchterlonie <stuarta@mythtv.org>
2011-06-22 13:09:35 +0100

    Update MySQL to 5.1.56

--- OSX/build/osx-packager.pl
+++ OSX/build/osx-packager.pl
@@ -167,7 +167,7 @@ our %depend = (
   'mysqlclient' =>
   {
     'url'
-    => 'http://mysql.he.net/Downloads/MySQL-5.1/mysql-5.1.54.tar.gz',
+    => 'http://mysql.he.net/Downloads/MySQL-5.1/mysql-5.1.56.tar.gz',
     'conf'
     =>  [
           '--without-debug',
