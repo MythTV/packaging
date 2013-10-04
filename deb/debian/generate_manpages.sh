9c9028114fb42d795c64ba745367db117662c5b8
Mario Limonciello <superm1@ubuntu.com>
2013-10-03 19:00:26 -0500

    deb: generate manpages during build

---
+++ deb/debian/generate_manpages.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=debian/tmp/usr/lib
+VERSION=`dpkg-parsechangelog | sed '/^Version/!d; s/.* //; s/.*://;'`
+generate()
+{
+	help2man --no-info -n "$1" --version-string="$VERSION" --no-discard-stderr "$2" > debian/man/`basename $2`.1
+}
+mkdir -p debian/man
+generate "MythTV AV Tester" debian/tmp/usr/bin/mythavtest
+generate "MythTV Commercial Flagger" debian/tmp/usr/bin/mythcommflag
+generate "MythTV FFMpeg Probe Debugger" debian/tmp/usr/bin/mythffprobe
+generate "MythTV Frontend (Real)"       debian/tmp/usr/bin/mythfrontend.real
+generate "MythTV LCD Server"     debian/tmp/usr/bin/mythlcdserver
+generate "MythTV Preview Generator" debian/tmp/usr/bin/mythpreviewgen
+generate "MythTV Shutdown helper" debian/tmp/usr/bin/mythshutdown
+generate "MythTV Utility helper" debian/tmp/usr/bin/mythutil
+generate "MythTV Backend" debian/tmp/usr/bin/mythbackend
+generate "MythTV FFMpeg helper " debian/tmp/usr/bin/mythffmpeg
+generate "MythTV FFMpeg server" debian/tmp/usr/bin/mythffserver
+generate "MythTV HDHomerun Configuration" debian/tmp/usr/bin/mythhdhomerun_config
+generate "MythTV Media Server" debian/tmp/usr/bin/mythmediaserver
+generate "MythTV replex" debian/tmp/usr/bin/mythreplex
+generate "MythTV Transcoder" debian/tmp/usr/bin/mythtranscode
+generate "MythTV welcome" debian/tmp/usr/bin/mythwelcome
+generate "MythTV closed caption extractor" debian/tmp/usr/bin/mythccextractor
+generate "MythTV ffmpeg player" debian/tmp/usr/bin/mythffplay
+generate "MythTV guide database filler" debian/tmp/usr/bin/mythfilldatabase
+generate "MythTV Job Queue manager" debian/tmp/usr/bin/mythjobqueue
+generate "MythTV metadata lookup" debian/tmp/usr/bin/mythmetadatalookup
+generate "MythTV Screen Wizard" debian/tmp/usr/bin/mythscreenwizard
+generate "MythTV Setup (real)" debian/tmp/usr/bin/mythtv-setup.real
+generate "MythTV Archive helper" debian/mytharchive/usr/bin/mytharchivehelper
+generate "MythTV Netvision helper" debian/mythnetvision/usr/bin/mythfillnetvision
+generate "MythTV Python shell" debian/libmyth-python/usr/bin/mythpython
+generate "MythTV wiki scripts" debian/libmyth-python/usr/bin/mythwikiscripts
+generate "MythTV Zoneminder server" debian/mythzoneminder/usr/bin/mythzmserver
