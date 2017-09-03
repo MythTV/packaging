bd635772cc62310eab80685cd9ed2721c94c046c
Thomas Mashos <thomas@mashos.com>
2017-09-02 17:02:02 -0700

    DEB: Update git hash checker for new version numbering

--- deb/debian/PPA-published-git-checker.py
+++ deb/debian/PPA-published-git-checker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys 
+import sys
 
 cachedir = "~/.launchpadlib/cache/"
 
@@ -9,20 +9,28 @@ launchpad = Launchpad.login_anonymously('mythbuntu PPA version checker', 'produc
 
 people = launchpad.people
 
-## Which team 
+## Which team
 mythbuntugroup = people['mythbuntu']
 
+version = sys.argv[1]
+
 ## Which PPA?
-archive = mythbuntugroup.getPPAByName(name=sys.argv[1])
+archive = mythbuntugroup.getPPAByName(name=version)
 
 ## Which source package?
 package=archive.getPublishedSources(source_name="mythtv")
+
+if version == '0.28':
+    splitnum = 4
+else:
+    splitnum = 3
 
 try:
     ## Print latest published source package
     fullversion=package[0].source_package_version
     ## Pull out GIT hash
-    hash = fullversion.split('-')[0].split('.')[4]
+    hash = fullversion.split('-')[0].split('.')[splitnum]
 except IndexError:
     hash = ''
+
 print hash
