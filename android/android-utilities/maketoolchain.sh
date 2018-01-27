18da6982418e85237dbaa02b59d34161c8b8f808
Mark Spieth <mspieth@digivation.com.au>
2018-01-28 09:39:29 +1100

    Create both toolchains, add options, help
    refs #12420

--- android/android-utilities/maketoolchain.sh
+++ android/android-utilities/maketoolchain.sh
@@ -1,7 +1,64 @@
 #!/bin/bash
 
+usage() {
+	cat <<END
+$1 [options]
+Options:
+	--force		force overwrite of existing toolchains
+	--sdkver	set sdk version, default 21
+	--gccversion	set gcc version, default 4.9, no others exist
+	--stl		set c++ stl to use: gnustl (default), libc++, stlport
+	--help		this help
+END
+}
+
+while : ; do
+	case "$1" in
+		--force)
+			shift
+			FORCE=--force
+			;;
+		--stl)
+			shift
+			STL=$1
+			shift
+			;;
+		--sdkver)
+			shift
+			SDKVERSION=$1
+			shift
+			;;
+		--gccver)
+			shift
+			GCCVERSION=$1
+			shift
+			;;
+		--help)
+			usage "$0"
+			exit 0
+			;;
+		-*)
+			echo "Invalid option: $1"
+			usage "$0"
+			exit 1
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
 [ -z "$SDKVERSION" ] && SDKVERSION=21
+[ -z "$STL" ] && STL=gnustl
 [ -z "$GCCVERSION" ] && GCCVERSION=4.9
 
 cd android-ndk
-build/tools/make-standalone-toolchain.sh --platform=android-$SDKVERSION --install-dir=`pwd`/my-android-toolchain --arch=arm --toolchain=arm-linux-androideabi-$GCCVERSION
+for i in "" 64; do
+	./build/tools/make-standalone-toolchain.sh \
+		--platform=android-$SDKVERSION \
+		--verbose \
+		--install-dir=`pwd`/my-android-toolchain$i \
+		--stl=$STL \
+		--arch=arm$i \
+		--toolchain=arm-linux-androideabi-$GCCVERSION $FORCE
+done
