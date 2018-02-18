4faef8d19faf484bcc30c55b438c3c2bf2eefb25
Mark Spieth <mspieth@digivation.com.au>
2018-02-18 14:31:07 +1100

    Android: Hide nav and status bar correctly
    To make them reappear, swipe from top.
    refs #12420

--- android/android-package-source/src/org/qtproject/qt5/android/bindings/QtActivity.java
+++ android/android-package-source/src/org/qtproject/qt5/android/bindings/QtActivity.java
@@ -285,12 +285,60 @@ public class QtActivity extends Activity
     }
     */
     //---------------------------------------------------------------------------
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        /*
+        View view = getWindow().peekDecorView();
+        if (view != null) {
+            view = view.getRootView();
+        }
+        */
+        View decorView = getWindow().getDecorView();
+        int uiOptions = 
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            uiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            uiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    // This snippet shows the system bars. It does this by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         onCreateHook(savedInstanceState);
+        hideSystemUI();
+
     }
     //---------------------------------------------------------------------------
 
@@ -653,6 +701,7 @@ public class QtActivity extends Activity
     protected void onResume()
     {
         super.onResume();
+        hideSystemUI();
         QtApplication.invokeDelegate();
     }
     //---------------------------------------------------------------------------
@@ -797,6 +846,9 @@ public class QtActivity extends Activity
     {
         if (!QtApplication.invokeDelegate(hasFocus).invoked)
             super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
         /*
         //View view = View.getRootView();
         View view = getWindow().peekDecorView();
@@ -811,6 +863,9 @@ public class QtActivity extends Activity
     public void super_onWindowFocusChanged(boolean hasFocus)
     {
         super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
         /*
         View view = getWindow().peekDecorView();
         if (view != null) {
