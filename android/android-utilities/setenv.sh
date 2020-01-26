0cf1fb78d300c7dabd772e45e3c36d55bc5af047
David Engel <david@istwok.net>
2020-01-26 14:48:47 -0600

    Fix for setenv.sh.

--- android/android-utilities/setenv.sh
+++ android/android-utilities/setenv.sh
@@ -12,7 +12,7 @@ export ANDROID_NDK=$ANDROID_ROOT/android-ndk
 export ANDROID_SDK_ROOT=$ANDROID_SDK
 export ANDROID_NDK_ROOT=$ANDROID_NDK
 if [ $(ls -1 "$ANDROID_SDK_ROOT/build-tools" | wc -l) == 1 ] ; then
-	export ANDROID_BUILD_TOOLS_REVISION=$(ls -1 android-sdk-linux/build-tools | grep "^[0-9]" | tail -1)
+	export ANDROID_BUILD_TOOLS_REVISION=$(ls -1 $ANDROID_SDK_ROOT/android-sdk-linux/build-tools | grep "^[0-9]" | tail -1)
 	echo "Using discovered tools version $ANDROID_BUILD_TOOLS_REVISION"
 else
 	export ANDROID_BUILD_TOOLS_REVISION=29.0.2
