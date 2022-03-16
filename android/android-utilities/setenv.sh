8558b3173d5fde81c4371bcff3d309f6ec1126bc
Peter Bennett <pbennett@mythtv.org>
2022-03-16 09:16:18 -0400

    android: Fixes for building with Qt 5.15.3
    1. Revert change to setenv.sh that caused a failure if you have only one
    ndk installed
    2. Comment <uses-sdk> tag in manifest, which casued packaging to fail
    with an error message.

--- android/android-utilities/setenv.sh
+++ android/android-utilities/setenv.sh
@@ -6,17 +6,12 @@ export ANDROID_SDK=$ANDROID_ROOT/Sdk
 export ANDROID_NDK=$ANDROID_ROOT/android-ndk
 export ANDROID_SDK_ROOT=$ANDROID_SDK
 export ANDROID_NDK_ROOT=$ANDROID_NDK
-if [ $(ls -1 "$ANDROID_SDK_ROOT/build-tools" | wc -l) -ge 1 ] ; then
+if [ $(ls -1 "$ANDROID_SDK_ROOT/build-tools" | wc -l) == 1 ] ; then
 	export ANDROID_BUILD_TOOLS_REVISION=$(ls -1 $ANDROID_SDK_ROOT/build-tools | grep "^[0-9]" | tail -1)
 	echo "Using discovered tools version $ANDROID_BUILD_TOOLS_REVISION"
 else
 	export ANDROID_BUILD_TOOLS_REVISION=29.0.2
 	echo "Using hardcoded tools version $ANDROID_BUILD_TOOLS_REVISION"
-fi
-if [ $(ls -1 "$ANDROID_SDK_ROOT/ndk" | wc -l) -ge 1 ] ; then
-	export ANDROID_NDK=$(ls -1 $ANDROID_SDK_ROOT/ndk | grep "^[0-9]" | tail -1)
-	echo "Using discovered ndk version $(basename $ANDROID_NDK)"
-	export ANDROID_NDK_ROOT=$ANDROID_NDK
 fi
 export JAVA_HOME=${ANDROID_ROOT}/android-studio/jre
 export JDK_PATH=${JAVA_HOME}/bin
