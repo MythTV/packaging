93167313bf7c0a8aa6f0f7aacfb2cf8fb285cbd8
jfabernathy <jfabernathy@gmail.com>
2022-08-20 07:09:59 -0400

    Update setenv.sh
    change sdk version for default to match readme.md

--- android/android-utilities/setenv.sh
+++ android/android-utilities/setenv.sh
@@ -10,7 +10,7 @@ if [ $(ls -1 "$ANDROID_SDK_ROOT/build-tools" | wc -l) == 1 ] ; then
 	export ANDROID_BUILD_TOOLS_REVISION=$(ls -1 $ANDROID_SDK_ROOT/build-tools | grep "^[0-9]" | tail -1)
 	echo "Using discovered tools version $ANDROID_BUILD_TOOLS_REVISION"
 else
-	export ANDROID_BUILD_TOOLS_REVISION=29.0.2
+	export ANDROID_BUILD_TOOLS_REVISION=29.0.3
 	echo "Using hardcoded tools version $ANDROID_BUILD_TOOLS_REVISION"
 fi
 export JAVA_HOME=${ANDROID_ROOT}/android-studio/jre
