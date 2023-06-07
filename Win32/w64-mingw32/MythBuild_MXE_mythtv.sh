ba9574b6b622e225db6f3e0963d858401582cc1b
Aidan <arobro@hotmail.com>
2023-06-07 19:58:07 +1200

    Refactor Windows build script
    1) The Windows build script has been moved from the main MythTv repo to
    the packaging repo, therefore a bunch of directories need to be updated.
    
    2) Bump GCC 8 to GCC 13.
    
    3) Add Support for building the x265 library
    
    4) Various code refactoring improvements to make the script more
    consistent with the other build scritps.
    
    5) Webkit patch for GCC 13
    
    The script has been updated for and tested on the latest LTS Ubuntu and
    can build the latest MythTV fixes branch.

--- Win32/w64-mingw32/MythBuild_MXE_mythtv.sh
+++ Win32/w64-mingw32/MythBuild_MXE_mythtv.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
 
-buildRoot=$PWD
-while test ! -e "$buildRoot/.git" ; do
-    buildRoot=${buildRoot%/*}
-    if test "x$buildRoot" == "x" ; then
-        echo "Cannot find root directory containing .git file or directory.  Exiting."
-        exit
-    fi
-done
-export buildRoot
-echo "Build root is $buildRoot"
+export MYMYTHPATH="`readlink -f ../../../mythtv`"
 
-echo "Settings paths"
-export buildPath=$buildRoot"/build"
-export PATH=$buildPath"/mxe/usr/bin":$PATH
-export qt5=$buildPath"/mxe/usr/i686-w64-mingw32.shared/qt5"
-export in1=$buildPath"/mxe/usr/i686-w64-mingw32.shared"
-
-if test "x$1" == "xclean" ; then
-    echo "Removing MythTV build directory"
-    rm -rf $buildPath/mythtv
+if [ ! -f "$MYMYTHPATH/authors.map" ]; then
+    echo "Cannot find MythTV source code."
+    exit
 fi
 
-if test -e "$buildPath/mythtv" ; then
-    echo "MythTV build directory exists"
-else
+export BASE=$PWD
+export BUILDPATH=$BASE"/build"
+export PATH=$BUILDPATH"/mxe/usr/bin":$PATH
+export QTMAKE=$BUILDPATH"/mxe/usr/bin"/i686-w64-mingw32.shared-qmake-qt5
+export IN1=$BUILDPATH"/mxe/usr/i686-w64-mingw32.shared"
+export CROSSPREFIX=$BUILDPATH"/mxe/usr/bin/"i686-w64-mingw32.shared
+
+function makeclean() {
+    echo "Cleaning MythTV build directory" rm - rf $BUILDPATH/windows-package-source
+
+    #setup new build source
+    rm -r -f $BUILDPATH/windows-package-source
+    mkdir -p $BUILDPATH/windows-package-source
+    cd $BASE
+    cp -r $MYMYTHPATH/. $BUILDPATH/windows-package-source
+    make clean
+}
+
+#process command
+case "$1" in
+    "clean")
+        makeclean
+        exit 0
+        ;;
+    "")
+        ;;
+    *)
+        echo "unknown command"
+        exit 2
+        ;;
+esac
+
+if [ ! -d "$BUILDPATH/windows-package-source" ]; then
     echo "Creating MythTV build directory"
-    mkdir -p $buildPath/mythtv
-    cd $buildRoot
-    cp -al mythplugins mythtv platform themestringstool $buildPath/mythtv
+    makeclean;
 fi
 
-cd $buildPath/mythtv
-if test -e .exif_patched ; then
+#fix mythtv music metadata scripts
+sudo mkdir -p /metadata/Music
+sudo chmod -R 777 /metadata/Music
+
+cd $BUILDPATH/windows-package-source
+export BUILDNAME=$(git describe --abbrev=4 HEAD)
+if test -e .exif_patched; then
     echo "libexif source already patched"
 else
     echo "Patching libexif source"
@@ -41,65 +59,78 @@ else
 fi
 
 echo "Compiling mythtv"
-cd $buildPath/mythtv/mythtv
-./configure --prefix="$buildPath/install" --enable-cross-compile --cross-prefix=i686-w64-mingw32.shared- --target_os=mingw32 --arch=x86 --cpu=pentium3 --qmake=$qt5/bin/qmake --extra-cflags=-I$in1/include-I/home/ubuntu/Desktop/build/mxe/usr/lib/gcc/i686-w64-mingw32.shared/8.4.0/include/c++/i686-w64-mingw32.shared --extra-ldflags=-L$in1/lib --disable-lirc --disable-hdhomerun --disable-firewire --disable-vdpau  --disable-nvdec --disable-dxva2 --enable-libmp3lame --enable-libx264 --enable-libx265 --enable-libxvid --enable-libvpx --disable-w32threads --enable-silent_cc
-if test $? != 0 ; then
+
+cd $BUILDPATH/windows-package-source/mythtv
+./configure --prefix="$BUILDPATH/install" --enable-cross-compile --cross-prefix=$CROSSPREFIX- --target_os=mingw32 --arch=x86 --cpu=pentium3 --qmake=$QTMAKE --extra-cflags=-I$IN1/include-I/home/ubuntu/Desktop/build/mxe/usr/lib/gcc/i686-w64-mingw32.shared/8.4.0/include/c++/i686-w64-mingw32.shared --extra-ldflags=-L$IN1/lib --disable-lirc --disable-hdhomerun --disable-firewire --disable-vdpau  --disable-nvdec --disable-dxva2 --enable-libmp3lame --enable-libx264 --enable-libx265 --enable-libxvid --enable-libvpx --disable-w32threads --enable-silent_cc
+if test $? != 0; then
     echo "Configure failed."
     exit
 fi
 
 make -j$(nproc)
-if test $? != 0 ; then
+if test $? != 0; then
     echo "Make failed."
     exit
 fi
 
-cp $buildPath/mythtv/mythtv/external/FFmpeg/ffmpeg_g.exe $buildPath/mythtv/mythtv/external/FFmpeg/mythffmpeg.exe
-cp $buildPath/mythtv/mythtv/external/FFmpeg/ffprobe_g.exe $buildPath/mythtv/mythtv/external/FFmpeg/mythffprobe.exe
+cp $BUILDPATH/windows-package-source/mythtv/external/FFmpeg/ffmpeg_g.exe $BUILDPATH/windows-package-source/mythtv/external/FFmpeg/mythffmpeg.exe
+cp $BUILDPATH/windows-package-source/mythtv/external/FFmpeg/ffprobe_g.exe $BUILDPATH/windows-package-source/mythtv/external/FFmpeg/mythffprobe.exe
 make install
-if test $? != 0 ; then
+if test $? != 0; then
     echo "Make install failed."
     exit
 fi
 
-# Hack warning. The libxxx.dll.a files get installed as liblibxxx.dll.a.
-cd $buildPath/install/lib
+#The libxxx.dll.a files get installed as liblibxxx.dll.a.
+cd $BUILDPATH/windows-package-source/install/lib
 mmv -d 'liblib*' 'lib#1'
 
-cd $buildPath/mythtv/mythplugins
-./configure --prefix="$buildPath/install" -cross-prefix=i686-w64-mingw32.shared- --disable-mytharchive
+cd $BUILDPATH/windows-package-source/mythplugins
+./configure --prefix="$BUILDPATH/install" --disable-mytharchive 
 make -j$(nproc) install
-if test $? != 0 ; then
+if test $? != 0; then
     echo "Make plugins failed."
     exit
 fi
 
-cp -R $buildPath/mythtv/platform/win32/w64-mingw32/Installer/. $buildPath/install/
-#cp -R $buildPath/mythtv/mythtv/src/COPYING $buildPath/install/COPYING
-rsync -a --exclude=".git" $buildPath/themes $buildPath/install/share/mythtv/
+cp -R $BASE/Installer/*.* $BUILDPATH/install/
+cp $BUILDPATH/windows-package-source/LICENSE $BUILDPATH/install/LICENSE
+rsync -a --exclude=".git" $BUILDPATH/themes $BUILDPATH/install/share/mythtv/
 
-cd $buildPath"/install/bin"
-
-mv opengl32.dll SOFTWARE_opengl32.dll
-rm opengl32.dll
-
-mv libmythgame.dll plugins/libmythgame.dll
-mv libmythnews.dll plugins/libmythnews.dll
-mv libmythmusic.dll plugins/libmythmusic.dll
-mv libmythbrowser.dll plugins/libmythbrowser.dll
-mv libmythzoneminder.dll plugins/libmythzoneminder.dll
-
+cd $BUILDPATH"/install/bin"
 mkdir -p platforms
 mkdir -p sqldrivers
-cp qwindows.dll $buildPath"/install/bin/platforms/windowplugin.dll"
-cp qwindowsvistastyle.dll $buildPath"/install/bin/platforms/qwindowsvistastyle.dll"
-cp qwindowsvistastyle.dll $buildPath"/install/bin/platforms/libtasn1-6.dll"
-cp qsqlmysql.dll $buildPath"/install/bin/sqldrivers/qsqlmysql.dll"
-cd ..
+mkdir -p plugins
 
+cp -R $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/qt5/bin/*.dll .
+cp -R $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/bin/*.dll .
+cp $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/qt5/plugins/sqldrivers/qsqlmysql.dll $BUILDPATH"/install/bin/sqldrivers/qsqlmysql.dll"
+cp $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/qt5/qml/QtQuick/Window.2/windowplugin.dll $BUILDPATH"/install/bin/platforms/windowplugin.dll"
+cp $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/qt5/plugins/styles/qwindowsvistastyle.dll $BUILDPATH"/install/bin/platforms/qwindowsvistastyle.dll"
+
+mv opengl32.dll SOFTWARE_opengl32.dll
+mv $BUILDPATH/install/lib/mythtv/plugins/* plugins
+mv $BUILDPATH/install/lib/*.dll .
+
+#remove version numbers from plugin file names
+cd $BUILDPATH/install/bin/plugins/
+for f in *.dll
+do
+    mv "$f" $(sed -e "s/[0-9]//g" <(echo $f))
+done
+
+cd $BUILDPATH/mxe/usr/i686-w64-mingw32.shared/qt5/plugins/
+find . -name \*.dll -exec cp {} $BUILDPATH"/install/bin/platforms/" \;
+
+cd $BUILDPATH"/install"
 find . -name \*.a -exec cp {} \lib \;
 find . -name \*.lib -exec cp {} \lib \;
+cd bin
+find . -name \*.a -exec cp {} \lib \;
+find . -name \*.lib -exec cp {} \lib \;
+cd ..
 
-zip -rq MythTv_Windows.zip *
+NOW=`date +"%d_%m_%Y"`
+zip -rq MythTV-Windows-$BUILDNAME-$NOW.zip *
 
-echo "Done"
+echo "Done building mythtv"
