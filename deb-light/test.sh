0158c57454467f6f61671d2d99718a284d85b009
Peter Bennett <pbennett@mythtv.org>
2025-03-12 11:09:38 -0400

    deb-light: Fix test.sh for alternate perl location
    (cherry picked from commit 84c38879631bee9ac1d9db1f5e7ea41a0f3d9b5f)

--- deb-light/test.sh
+++ deb-light/test.sh
@@ -27,6 +27,10 @@ project=$(basename $(dirname "$longname"))
 case $project in
     mythtv|mythplugins)
         basedir=$destdir/usr
+        perl5=`ls -d $basedir/local/share/perl/*||true`
+        if [[ "$perl5" == "" ]]; then
+            perl5=`ls -d $basedir/share/perl/*||true`
+        fi
         set -x
         export PATH=$basedir/bin:$basedir/local/bin:$PATH
         export MYTHTVDIR=$basedir
@@ -34,7 +38,7 @@ case $project in
         export MYTHCONFDIR=$HOME/.mythtv-$shortname
         # dist-packages if installed by debian
         export PYTHONPATH=`ls -d $basedir/local/lib/python*/dist-packages`
-        export PERL5LIB=`ls -d $basedir/local/share/perl/*`${PERL5LIB:+:${PERL5LIB}}
+        export PERL5LIB=$perl5${PERL5LIB:+:${PERL5LIB}}
         exec $pgm "$@"
         ;;
     *)
